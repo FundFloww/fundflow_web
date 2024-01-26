a4af1a555a4429fe01561c9a5fa383655f068242
JDurendez <javier.durendez@iesdoctorbalmis.com>
2024-01-26 13:04:51 +0100

    Rutas arregladas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,16 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
 import { InicioComponent } from './inicio/inicio.component';
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
     InicioComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    // RouterModule
-  ],
-  exports: [
-    // RouterModule
-  ],
-  bootstrap: [AppComponent]
+  imports: [],
+  exports: [],
+  bootstrap: []
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { PerfilEmprendedorComponent } from './perfil-emprendedor/perfil-emprendedor.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { IdeaComponent } from './idea/idea.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'inicio', component: InicioComponent },
+    { path: 'perfil', component: PerfilEmprendedorComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'registro', component: RegistroComponent },
+    { path: 'idea/:id', component: IdeaComponent },
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+    { path: '**', redirectTo: '/inicio', pathMatch: 'full' }
+];
