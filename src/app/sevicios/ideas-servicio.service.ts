8aa74633932977845b7e838a50fd286b49c7d40e
Mario Esparza <hp151lolxd@gmail.com>
2024-01-30 18:48:10 +0100

    Cards de ideas acabadas con resposive y aplicado en inicio

--- src/app/sevicios/ideas-servicio.service.ts
+++ src/app/sevicios/ideas-servicio.service.ts
@@ -2,34 +2,45 @@ import { Injectable } from '@angular/core';
 import { Idea } from '../interfaces/idea';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class IdeasServicioService {
-  private ideas: Idea[] = [{
-    id: 1,
-    titulo: 'Fund Flow',
-    campo: 'IT',
-    imagenes: [],
-    descripcion: 'Idea de prueba 1'
-  } , {
-    id: 2,
-    titulo: 'Idea 2',
-    campo: 'Salud',
-    imagenes: [],
-    descripcion: 'Idea de prueba 2'
-  } , {
-    id: 3,
-    titulo: 'Otra idea',
-    campo: 'Otro',
-    imagenes: [],
-    descripcion: 'Una idea adicional para pruebas manuales y como quiero probar cosas con la longitud añada más texto superinteresante, para nada es relleno.'
-  }];
+    private ideas: Idea[] = [
+        {
+            id: 1,
+            titulo: 'Fund Flow',
+            campo: 'IT',
+            imagenes: [],
+            descripcion: 'Idea de prueba 1'
+        }, 
+        {
+            id: 2,
+            titulo: 'Idea 2',
+            campo: 'Salud',
+            imagenes: [],
+            descripcion: 'Idea de prueba 2'
+        },
+        {
+            id: 3,
+            titulo: 'Otra idea',
+            campo: 'Otro',
+            imagenes: [],
+            descripcion: 'Una idea adicional para pruebas manuales y como quiero probar cosas con la longitud añada más texto superinteresante, para nada es relleno.'
+        },
+        {
+            id: 4,
+            titulo: 'Otra idea mas',
+            campo: 'Otro',
+            imagenes: [],
+            descripcion: 'Una idea adicional para pruebas manuales y como quiero probar cosas con la longitud añada más texto superinteresante, para nada es relleno.'
+        }
+    ];
 
-  getIdeas(): Idea[] {
-    return this.ideas;
-  }
+    getIdeas(): Idea[] {
+        return this.ideas;
+    }
 
-  addIdea(idea: Idea) {
-    this.ideas.push(idea);
-  }
+    addIdea(idea: Idea) {
+        this.ideas.push(idea);
+    }
 }
