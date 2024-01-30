45eb89c127a138a1a71584cf07157b9aef6a1d08
DrM4r1o <mario.esparza@iesdoctorbalmis.com>
2024-01-30 12:36:12 +0100

    Ideas añadidas a inicio

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from '../side-bar/side-bar.component';
+import { IdeasServicioService } from '../sevicios/ideas-servicio.service';
+import { Idea } from '../interfaces/idea';
+import { IdeaItemComponent } from '../idea-item/idea-item.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [SideBarComponent],
+  imports: [SideBarComponent, IdeaItemComponent],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
 export class InicioComponent {
+    ideas: Idea[] = [];
 
+    constructor(private ideaService: IdeasServicioService) { }
+    
+    ngOnInit() {
+      this.ideas = this.ideaService.getIdeas();
+    }
 }
