cacb1278fa495e16a125a5e7f4a7606f05229db2
Ainhoa Gomez <ainhoa.gomez@iesdoctorbalmis.com>
2024-02-06 14:29:08 +0100

    Gestion de errores de registro sin terminar

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -2,60 +2,66 @@ import { Component } from '@angular/core';
 import { UsuariosService } from '../servicios/usuarios.service';
 import { Router } from '@angular/router';
 import { UsuarioRegistroDTO } from "../interfaces/usuario-registro-dto";
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { TipoUsuario } from '../enum/tipo-usuario';
 
 @Component({
-  selector: 'app-registro',
-  standalone: true,
-  imports: [FormsModule, CommonModule],
-  templateUrl: './registro.component.html',
-  styleUrl: './registro.component.scss'
+    selector: 'app-registro',
+    standalone: true,
+    imports: [FormsModule, CommonModule],
+    templateUrl: './registro.component.html',
+    styleUrl: './registro.component.scss'
 })
 export class RegistroComponent {
-  constructor(
-    private usuariosService: UsuariosService,
-    private router: Router) { }
+    constructor(
+        private usuariosService: UsuariosService,
+        private router: Router) { }
 
-  newUsuario: UsuarioRegistroDTO = {
-    nombre: '',
-    apellidos: '',
-    correo: '',
-    contrasena: '',
-    confirmarContrasena: '',
-    tipo: 'EMPRENDEDOR'
-  };
+    newUsuario: UsuarioRegistroDTO = {
+        nombre: '',
+        apellidos: '',
+        correo: '',
+        contrasena: '',
+        confirmarContrasena: '',
+        tipo: 'EMPRENDEDOR'
+    };
 
-  errorContrasena: string = '';
-  showError: boolean = false;
-  userExists: boolean = false;
+    errorContrasena: string = '';
+    showError: boolean = false;
+    userExists: boolean = false;
 
-  goBack() {
-    this.router.navigate(['/login']);
-  }
-
-  async addNewUsuario() {
-    if (!this.contrasenasCoinciden()) {
-      this.showError = true;
-      return;
+    goBack() {
+        this.router.navigate(['/login']);
     }
 
-    this.userExists = await this.usuariosService.addUsuario(this.newUsuario);
+    async addNewUsuario() {
+        if (!this.contrasenasCoinciden()) {
+            this.showError = true;
+            return;
+        }
 
-    if(!this.userExists){
-      this.goBack();
-    }
-  }
+        this.userExists = await this.usuariosService.addUsuario(this.newUsuario);
 
-  contrasenasCoinciden(): boolean {
-    if (this.newUsuario.contrasena !== this.newUsuario.confirmarContrasena) {
-      this.errorContrasena = 'Las contraseñas no coinciden';
-      return false;
-    } else {
-      this.errorContrasena = '';
-      return true;
+        if (!this.userExists) {
+            this.goBack();
+        }
     }
-  }
+
+    contrasenasCoinciden(): boolean {
+        if (this.newUsuario.contrasena !== this.newUsuario.confirmarContrasena) {
+            this.errorContrasena = 'Las contraseñas no coinciden';
+            return false;
+        } else {
+            this.errorContrasena = '';
+            return true;
+        }
+    }
+
+    validClasses(ngModel: NgModel, validClass: string, errorClass: string) {
+        return {
+            [validClass]: ngModel.touched && ngModel.valid,
+            [errorClass]: ngModel.touched && ngModel.invalid
+        };
+    }
 
 }
