df2e064b865d1556757a1a742be31dfd11a332b9
Ainhoa Gomez <ainhoa.gomez@iesdoctorbalmis.com>
2024-02-08 11:42:43 +0100

    Filtros creo que terminados

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { camposKeys } from '../enum/campos';
+import { camposKeys, Campos } from '../enum/campos';
 import { IdeaItemComponent } from '../idea-item/idea-item.component';
 import { IdeaDto } from '../interfaces/ideaDto';
 import { IdeasServicioService } from '../servicios/ideas-servicio.service';
@@ -20,6 +20,7 @@ export class InicioComponent {
     camposArray: string[] = camposKeys;
     open: boolean = true;
     session: boolean | null = null;
+    camposSeleccionados: string[] = [];
 
     constructor(
         private ideaService: IdeasServicioService, 
@@ -38,5 +39,31 @@ export class InicioComponent {
 
     onOpenBar() {
         this.open = onOpenBarFunction(this.open);
+    }
+
+    toggleCampo(campo: string) {
+        if (this.camposSeleccionados.includes(campo)) {
+            this.camposSeleccionados = this.camposSeleccionados.filter(c => c !== campo);
+            console.log(this.camposSeleccionados);
+        } else {
+            this.camposSeleccionados.push(campo);
+            console.log(this.camposSeleccionados);
+        }
+
+        // this.filtrarIdeas();
+    }
+
+    async filtrarIdeas() {
+        try {
+            const camposEnum: Campos[] = this.camposSeleccionados.map(campo => Campos[campo as keyof typeof Campos]);
+
+            this.ideas = await this.ideaService.getIdeasFiltradas(camposEnum);
+        } catch (error) {
+            console.error("Ocurrió un error al filtrar las ideas: ", error);
+        }
+    }
+
+    isSelected(campo: string): boolean {
+        return this.camposSeleccionados.includes(campo);
     }
 }
