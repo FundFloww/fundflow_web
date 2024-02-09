13851ab8c309c7b7c8285df588065c0f6de9f03a
Ainhoa Gomez <ainhoa.gomez@iesdoctorbalmis.com>
2024-02-09 08:32:10 +0100

    Carpeta environments creada

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    baseUrl: "http://10.100.11.1:9000"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    baseUrl: "http://10.100.11.1:9000"
+};
