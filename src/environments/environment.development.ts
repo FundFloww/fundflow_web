0e234741e636272d6c0639587f9fafe4790177ea
Ainhoa Gomez <ainhoa.gomez@iesdoctorbalmis.com>
2024-02-09 09:00:16 +0100

    #

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    baseUrl: "http://10.100.12.1:9000"
+    baseUrl: "http://10.100.12.1:9000",
+    blobPassword: ""
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    baseUrl: "http://10.100.12.1:9000"
+    baseUrl: "http://10.100.12.1:9000",
+    blobPassword: ""
 };
