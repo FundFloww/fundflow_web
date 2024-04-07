7a2e145171a9c60382aea4b2693a11da3e0d9b1f
Juandi <juandsolerc@gmail.com>
2024-04-07 23:11:25 +0200

    #

--- src/app/pages/zona-admin-ideas/zona-admin-ideas.component.ts
+++ src/app/pages/zona-admin-ideas/zona-admin-ideas.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-zona-admin-ideas',
-  standalone: true,
-  imports: [],
-  templateUrl: './zona-admin-ideas.component.html',
-  styleUrl: './zona-admin-ideas.component.scss'
+	selector: 'zona-admin-ideas',
+	standalone: true,
+	imports: [
+		FormsModule,
+		CommonModule
+	],
+	templateUrl: './zona-admin-ideas.component.html',
+	styleUrl: './zona-admin-ideas.component.scss'
 })
 export class ZonaAdminIdeasComponent {
 
