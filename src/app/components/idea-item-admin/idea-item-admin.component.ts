bf6c50f57c225d83bbaef5f21abc95248d3f89ca
Juandi <juandsolerc@gmail.com>
2024-05-12 22:37:05 +0200

    Cambios de estilos en la card de la idea en zona admin

--- src/app/components/idea-item-admin/idea-item-admin.component.ts
+++ src/app/components/idea-item-admin/idea-item-admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { IdeaDto } from '../../interfaces/ideaDto';
 import { Usuario } from '../../interfaces/usuario';
+import { UsuariosService } from '../../services/usuarios/usuarios.service';
 
 @Component({
 	selector: 'idea-item-admin',
@@ -13,5 +14,13 @@ export class IdeaItemAdminComponent {
 
 	@Input() idea!: IdeaDto;
 	usuario: Usuario | null = null;
+
+	constructor(
+        private usuariosService: UsuariosService
+    ) { }
+
+    async ngOnInit() {
+        this.usuario = await this.usuariosService.getUsuarioPorId(this.idea.emprendedor[0].id ?? 0);
+    }
 
 }
